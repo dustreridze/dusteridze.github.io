a62089947cc18141d292967c995b0fc895d97417
dustreridze <151583642+dustreridze@users.noreply.github.com>
2023-12-07 02:39:58 +0300

    Delete assets/scripts/main.js

--- assets/scripts/main.js
+++
@@ -1,9 +0,0 @@
-isMenuCities = false;
-
-const open_cities = () => {
-	let pick_city = document.querySelector('.pick-city');
-	
-	isMenuCities ? pick_city.classList.remove('active') : pick_city.classList.add('active');
-
-	isMenuCities = !isMenuCities;
-}
